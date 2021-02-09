8c421b17a967cd8cea509313f51896d775cabcb1
Abraham Torres Pinto <abraham.torres@Abraham-Torres-Pinto-MacBook-Pro.local>
2021-02-08 20:59:40 -0500

    Added logic to support the addition of new tasks

---
+++ app.js
@@ -0,0 +1,46 @@
+// Defining UI variables
+const form = document.querySelector("#task-form");
+const tasksList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Creating function loadAllEventListeners
+function loadEventListeners() {
+    // Add task event
+    form.addEventListener("submit", addTask);
+
+};
+
+// Add task
+function addTask(e) {
+    if(taskInput.value === "") {
+        alert("Please enter a task");
+    }
+
+    // Create <li> element
+    const li = document.createElement("li");
+    // Add class
+    li.className = "collection-item yellow darken-2";
+    // Create text node and append to the <li>
+    li.appendChild(document.createTextNode(taskInput.value))
+    // Create new link element
+    const link = document.createElement("a");
+    // Add class
+    link.className = "delete-item secondary-content";
+    // Add icon html
+    link.innerHTML = "<i class='fa fa-remove'></i>";
+    // Append link to the <li>
+    li.appendChild(link);
+
+    // Append the <li> to the <ul>
+    tasksList.appendChild(li);
+
+    // Clear input
+    taskInput.value = "";
+
+    e.preventDefault();
+}
