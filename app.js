663d0451a6bafefb9f28caa4f0678f507009645a
Abraham Torres Pinto <abraham.torres@Abraham-Torres-Pinto-MacBook-Pro.local>
2021-02-12 17:41:45 -0500

    Added functionality to persist in Local Storage

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Creating function loadEventListeners
 function loadEventListeners() {
+    // DOM Load Event
+    document.addEventListener("DOMContentLoaded", getTasks);
     // Add task event
     form.addEventListener("submit", addTask);
     // Remove task event
@@ -20,9 +22,39 @@ function loadEventListeners() {
     filter.addEventListener("keyup", filterTasks);
 };
 
+// Get Tasks from LocalStorage
+function getTasks() {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function (task) {
+        // Create <li> element
+        const li = document.createElement("li");
+        // Add class
+        li.className = "collection-item yellow darken-2";
+        // Create text node and append to the <li>
+        li.appendChild(document.createTextNode(task))
+        // Create new link element
+        const link = document.createElement("a");
+        // Add class
+        link.className = "delete-item secondary-content";
+        // Add icon html
+        link.innerHTML = "<i class='fa fa-remove'></i>";
+        // Append link to the <li>
+        li.appendChild(link);
+
+        // Append the <li> to the <ul>
+        tasksList.appendChild(li);
+    })
+}
+
 // Add task
 function addTask(e) {
-    if(taskInput.value === "") {
+    if (taskInput.value === "") {
         alert("Please enter a task");
     }
 
@@ -44,36 +76,81 @@ function addTask(e) {
     // Append the <li> to the <ul>
     tasksList.appendChild(li);
 
+    // Store in Local Storage
+    storeTasksInLocalStorage(taskInput.value);
+
     // Clear input
     taskInput.value = "";
 
     e.preventDefault();
 }
 
+// Store Tasks In Local Storage
+function storeTasksInLocalStorage(task) {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 // Remove Task
 function removeTask(e) {
-    if(e.target.parentElement.classList.contains("delete-item")) {
-        if(confirm("Are you sure you want to delete this task?")){
+    if (e.target.parentElement.classList.contains("delete-item")) {
+        if (confirm("Are you sure you want to delete this task?")) {
             e.target.parentElement.parentElement.remove();
+
+            // Remove from LocalStorage
+            removeTasksFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
 }
 
+// Remove From LocalStorage
+function removeTasksFromLocalStorage(taskItem) {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function(task, index) {
+        if(taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 // Clear All Tasks
 function clearTasks() {
-    while(tasksList.firstChild) {
+    while (tasksList.firstChild) {
         tasksList.removeChild(tasksList.firstChild);
     }
+    // Clear Tasks From Local Storage
+    clearTasksFromLocalStorage();
+}
+
+// Clear Tasks From Local Storage
+function clearTasksFromLocalStorage() {
+    localStorage.clear();
 }
 
 // Filter Tasks
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
-  
+
     document.querySelectorAll(".collection-item").forEach(
-        function(task) {
+        function (task) {
             const item = task.firstChild.textContent;
-            if(item.toLowerCase().indexOf(text) != -1){
+            if (item.toLowerCase().indexOf(text) != -1) {
                 task.style.display = "Block";
             } else {
                 task.style.display = "None";
@@ -81,3 +158,4 @@ function filterTasks(e) {
         }
     );
 }
+
